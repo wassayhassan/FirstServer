bac5063442c29d6fa635ccdcca0b22b2ae59c646
wassayhassan <102725500+wassayhassan@users.noreply.github.com>
2023-01-17 01:02:42 +0500

    0.0.2

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.listen(8000, ()=> {
     console.log("listening at port 8080");
 })
-app.get("/", (req, res)=> {
+app.get("/", async(req, res)=> {
     res.send("Server is working");
+})
+app.get('/hello', async(req, res)=> {
+    res.send("hello, there");
 })
